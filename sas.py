fb84bb7ab7024649af377e545863a37479201441
Mehmet Karahan Çetinkaya <71176654+karahanOS@users.noreply.github.com>
2024-09-07 18:07:46 +0300

    Update sas.py

--- sas.py
+++ sas.py
@@ -4,6 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 # Use the same model and tokenizer for consistency
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
@@ -11,10 +15,15 @@ model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-mu
 
 # Load the reviews dataset
 df_reviews = pd.read_csv(r'C:\Users\Karahan C\Desktop\Portfolio Projects\MARKETING\CSV FILES\FACT_Reviews.csv')
+df_reviews.info()           
+df_reviews.describe()
+print(df_reviews.head())
+
+
 
 # Function to get sentiment score from review text
 def sentiment_score(review):
-    tokens = tokenizer.encode(review, return_tensors='pt',clean_up_tokenization_spaces=True)
+    tokens = tokenizer.encode(review, return_tensors='pt', max_length=1024, truncation=True)
     result = model(tokens)
     return int(torch.argmax(result.logits)) + 1
 
@@ -22,9 +31,9 @@ def sentiment_score(review):
 df_reviews['sas'] = df_reviews['ReviewText'].apply(lambda x: sentiment_score(x))
 
 
-
-
-
 # df_reviews.to_csv(r'C:\Users\Karahan C\Desktop\Portfolio Projects\MARKETING\CSV FILES\Processed_Reviews.csv', index=False)
 
-print(df_reviews.head())
+# sns.pairplot(data=df_reviews, vars=['Rating', 'sas'], hue='sas')
+# plt.show()            
+
+print(df_reviews.query('Rating==1').sort_values('sas', ascending=False)['ReviewText'].values[0])          
